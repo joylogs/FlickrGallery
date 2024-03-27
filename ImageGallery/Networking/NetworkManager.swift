4d892431e7fd252127e12a1a13e38c4ae9122659
Joy <joy.logs@gmail.com>
2024-03-27 23:48:20 +0530

    Networking layer separated

---
+++ ImageGallery/Networking/NetworkManager.swift
@@ -0,0 +1,11 @@
+//
+//  NetworkManager.swift
+//  ImageGallery
+//
+//  Created by Joy Banerjee on 27/03/24.
+//  Copyright © 2024 Joy Banerjee. All rights reserved.
+//
+
+import Foundation
+
+
--- ImageGallery/Networking/NetworkRouter.swift
+++ ImageGallery/Networking/NetworkRouter.swift
@@ -8,13 +8,42 @@
 
 import Foundation
 
-typealias Completion = ()->()
-
-protocol NetworkBindable {
-    associatedtype EndPoint = EndPointType
-    func handleAPIRequest(_ endPoint: EndPoint, _ completion: @escaping Completion)
+struct Test: Codable {
+    
 }
 
-struct NetworkRouter: NetworkBindable {
-    
+typealias Completion<T> = (_ json: T?, _ error: Error?) -> ()
+
+protocol NetworkBindable {
+    associatedtype EndPoint: EndPointType
+    func handleAPIRequest<T: Codable>(_ endPoint: EndPoint, _ completion: @escaping Completion<T>)
+}
+
+struct NetworkRouter<EndPoint: EndPointType>: NetworkBindable {
+    func handleAPIRequest<T: Codable>(_ endPoint: EndPoint, _ completion: @escaping Completion<T>) {
+        
+        if let request = buildRequest(endPoint) {
+            URLSession.shared.dataTask(with: request) { data, urlResponse, error in
+                if let useableData = data {
+                    let decoder = JSONDecoder()
+                    do {
+                        let jsonData = try decoder.decode(T.self, from: useableData)
+                        completion(jsonData, nil)
+                    } catch(let error) {
+                        print(error.localizedDescription)
+                        completion(nil, error)
+                    }
+                }
+            }
+        }
+    }
+}
+
+
+private extension NetworkRouter {
+    func buildRequest(_ endPoint: EndPoint) -> URLRequest? {
+        guard let url = URL(string: endPoint.path, relativeTo: endPoint.baseURL) else { return nil }
+        let request = URLRequest(url: url, cachePolicy: .reloadIgnoringCacheData, timeoutInterval: 100)
+        return request
+    }
 }
